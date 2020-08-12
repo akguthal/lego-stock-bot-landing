99911b435adf8e0f662c89ffef8b4ba1879a0b09
Akshay Guthal <akguthal@gmail.com>
2020-08-11 21:40:18 -0400

    initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          LEGO Stock Bot
+        </h1>
+        <p>Coming soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
